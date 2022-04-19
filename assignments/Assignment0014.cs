06e8d145ad21dda433f086c7605190c722c7121d
sourcx <x@e01.nl>
2022-04-19 21:50:49 +0200

    0014

---
+++ assignments/Assignment0014.cs
@@ -0,0 +1,44 @@
+
+using System.Text;
+
+namespace leetcode
+{
+    class Assignment0014
+    {
+        public static void Run()
+        {
+            System.Console.WriteLine(new Assignment0014().LongestCommonPrefix(new string[] { "flower","f" }));
+        }
+
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length < 1)
+            {
+                return "";
+            }
+
+            var first = strs[0];
+            var res = new StringBuilder();
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    if (i == strs[j].Length)
+                    {
+                        return res.ToString();
+                    }
+
+                    if (strs[j][i] != first[i])
+                    {
+                        return res.ToString();
+                    }
+                }
+
+                res.Append(first[i]);
+            }
+
+            return res.ToString();
+        }
+    }
+}
