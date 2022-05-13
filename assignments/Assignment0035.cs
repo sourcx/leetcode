910e736a936b46d5f9fa29b94409b0e87b460f88
sourcx <25772871+sourcx@users.noreply.github.com>
2022-05-13 07:31:47 +0200

    0035

---
+++ assignments/Assignment0035.cs
@@ -0,0 +1,45 @@
+namespace leetcode
+{
+    class Assignment0035
+    {
+        public static void Run()
+        {
+            var nums = new[] { 1,3,5,6 };
+            var target = 10;
+            var res = new Assignment0035().SearchInsert(nums, target);
+            System.Console.WriteLine($"Answer: {res}.");
+        }
+
+        public int SearchInsert(int[] nums, int target)
+        {
+            int min = 0;
+            int max = nums.Length - 1;
+
+            int mid = min + (max - min) / 2;
+
+            while (min < max)
+            {
+                if (target == nums[mid])
+                {
+                    return mid;
+                }
+                else if (target > nums[mid])
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid;
+                }
+                mid = min + (max - min) / 2;
+            }
+
+            if (target > nums[mid])
+            {
+                return mid + 1;
+            }
+
+            return mid;
+        }
+    }
+}
